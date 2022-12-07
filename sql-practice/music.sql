d3ed94c5a9a6f1a00b4c800f781b1029e2b4e4fc
ChrisCarucci <Chris.w.carucci@gmail.com>
2022-12-07 01:24:07 -0330

    One to many and many to many table creation.

---
+++ sql-practice/music.sql
@@ -0,0 +1,21 @@
+-- This ensures that SQLite enforces FOREIGN KEY constraints
+PRAGMA foreign_keys = 1;
+DROP TABLE IF EXISTS instruments;
+DROP TABLE IF EXISTS musicians;
+DROP TABLE IF EXISTS bands;
+
+CREATE TABLE bands (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR(100)
+);
+CREATE TABLE musicians (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100),
+  band_id integer not null,
+  foreign key (band_id) references bands(id)
+);
+CREATE TABLE instruments (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  type VARCHAR(100) NOT NULL
+);
